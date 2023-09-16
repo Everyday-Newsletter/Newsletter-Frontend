5f7967b7a93262e236de7933efaac5a03a37950a
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-09-16 11:50:33 -0400

    format

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -8,47 +8,50 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-export default function Root() {
+export default function Newsletter() {
   const [loading, setLoading] = React.useState(false);
 
-  const formSubmit = (event) => {
-    console.log(event);
-    event.preventDefault();
+  const formSubmit = (/* event */) => {
+    // console.log(event);
+    // event.preventDefault();
     setLoading(true);
 
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
+    // setTimeout(() => {
+    // setLoading(false);
+    // }, 2000);
   };
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h3" align="center" color="inherit" noWrap>
-        Good Morning
-      </Typography>
-      <Stack
-        component="form"
-        sx={{ pt: 4 }}
-        direction="row"
-        spacing={2}
-        justifyContent="center"
-        onSubmit={formSubmit}
-      >
-        <TextField
-          id="outlined-basic"
-          label="How are you today?"
-          name="feelings"
-          fullWidth
-          variant="outlined"
-        />
-        <Button variant="contained" type="submit" disabled={loading}>
-          {loading ? (
-            <CircularProgress color="inherit" size={30} />
-          ) : (
-            <SendIcon />
-          )}
-        </Button>
-      </Stack>
-    </Container>
+    <>
+      <Container maxWidth="sm">
+        <Typography variant="h3" align="center" color="inherit" noWrap>
+          Good Morning
+        </Typography>
+        <Stack
+          component="form"
+          action="/news"
+          sx={{ pt: 4 }}
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+          onSubmit={formSubmit}
+        >
+          <TextField
+            id="outlined-basic"
+            label="How are you today?"
+            name="feelings"
+            fullWidth
+            variant="outlined"
+          />
+          <Button variant="contained" type="submit" disabled={loading}>
+            {loading ? (
+              <CircularProgress color="inherit" size={30} />
+            ) : (
+              <SendIcon />
+            )}
+          </Button>
+        </Stack>
+      </Container>
+    </>
   );
 }
